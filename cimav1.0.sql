1e76cc5b66fd9098d801afb865e74e293e991905
panduzpan <143237292+panduzpan@users.noreply.github.com>
2025-03-06 16:38:49 -0500

    Add files via upload

---
+++ cimav1.0.sql
@@ -0,0 +1,129 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-03-2025 a las 01:03:05
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cimav`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grabacion`
+--
+
+CREATE TABLE `grabacion` (
+  `id_grabacion` int(30) NOT NULL,
+  `grabacion hd por vmix` varchar(100) NOT NULL,
+  `grabacion full hd por vmix` varchar(100) NOT NULL,
+  `grabacion 4k por vmix` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rumba`
+--
+
+CREATE TABLE `rumba` (
+  `id_rumba` int(30) NOT NULL,
+  `miniteca` varchar(100) NOT NULL,
+  `boda` varchar(100) NOT NULL,
+  `15 años` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicio`
+--
+
+CREATE TABLE `servicio` (
+  `id_servicio` int(30) NOT NULL,
+  `grabacion` int(100) NOT NULL,
+  `transmision` int(100) NOT NULL,
+  `rumba` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `streaming`
+--
+
+CREATE TABLE `streaming` (
+  `id_streaming` int(30) NOT NULL,
+  `grab y trans por zoom` varchar(100) NOT NULL,
+  `grab y trans por meet` varchar(100) NOT NULL,
+  `grab y trans por teams` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(30) NOT NULL,
+  `correo` varchar(30) NOT NULL,
+  `contraseña` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `grabacion`
+--
+ALTER TABLE `grabacion`
+  ADD PRIMARY KEY (`id_grabacion`);
+
+--
+-- Indices de la tabla `rumba`
+--
+ALTER TABLE `rumba`
+  ADD PRIMARY KEY (`id_rumba`);
+
+--
+-- Indices de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD PRIMARY KEY (`id_servicio`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD CONSTRAINT `servicio_ibfk_1` FOREIGN KEY (`id_servicio`) REFERENCES `usuario` (`id_usuario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
